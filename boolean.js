3474cbbcc3c424ab8547d22e1fc109588569d1aa
PontusSven <pontussvensson92@gmail.com>
2019-01-21 09:36:36 +0100

    booleans

---
+++ boolean.js
@@ -0,0 +1,30 @@
+
+// === equality operator ( !== )
+// < - less than operator
+// > - greater than operator
+// <= - less than or equalt to
+// >= - greater than or equal to
+
+
+let temp = 32;
+let isFreezing = temp === 32;
+
+console.log('Is it freezing? ' + isFreezing);
+
+
+// Challenge
+
+// Create age set to your age
+let myAge = 26;
+
+// Calculate is chiild - if they are 7 or under
+let isChild = myAge <= 7;
+
+// Calculate is senior - if they are 65 or older
+let isSenior = myAge >= 65;
+
+// print is child value
+console.log('Child is ' + isChild);
+
+// print is senior value
+console.log('Senior is ' + isSenior);
