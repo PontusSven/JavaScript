8a4ebd1385bf03f331f1bacfb85cca25dc0e5deb
PontusSven <pontussvensson92@gmail.com>
2019-01-21 09:02:42 +0100

    Temp conversion

---
+++ temp-conversion.js
@@ -0,0 +1,11 @@
+// farenheit
+let farenheit = 50;
+
+// celcius
+let celcius = (farenheit - 32) * 5/9;
+console.log('Celcius: ' + celcius);
+
+// kelvin
+let kelvin = (farenheit + 459.67) * 5/9;
+console.log('Kelvin: ' + kelvin);
+
