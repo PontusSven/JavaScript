3d5c510afd218b40eb75d24094e479196d947a0d
Pontus Svensson <Pontus.Svensson@hedinit.se>
2019-01-25 13:44:37 +0100

    Scope 2 variable shadowing

---
+++ scope-2.js
@@ -0,0 +1,20 @@
+// Global ('Pontus')
+    // Local ('Svensson')
+        // Local (*Kajsa*)
+    // Local 
+    
+let name = 'Pontus';
+
+if (true) {
+    let name = 'Svensson'
+    console.log(name);
+
+    if (true) {
+        let name = 'Kajsa';
+        console.log(name);
+    }
+}
+
+if (true) {
+    console.log(name);
+}
