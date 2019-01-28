cc9dc0a1f0944c5323aaa81d34ddc8bb9f18624f
Pontus Svensson <Pontus.Svensson@hedinit.se>
2019-01-28 09:44:00 +0100

    undefined / null

---
+++ undefined-null.js
@@ -0,0 +1,26 @@
+// Undefined for variable
+let name
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+// Undefined for funtion arguments
+// Undefined as the function return default value
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square();
+
+console.log(result);
+
+// 
+let age = 26;
+
+age = null;
+
+console.log(age);
+
