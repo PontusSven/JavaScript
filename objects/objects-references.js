d0b8179c9e8573d0be55751ff84e5c1de2ca2306
Pontus Svensson <Pontus.Svensson@hedinit.se>
2019-01-30 15:57:39 +0100

    object references

---
+++ objects/objects-references.js
@@ -0,0 +1,19 @@
+let myAccount = {
+    name: 'Pontus Svensson',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function (account, amount) {
+    account.expenses = account.expenses + amount
+    console.log(account)
+}
+
+let addIncome = function (account, amount) {
+    account.income = account.income + amount
+    console.log(account)
+}
+
+addExpense(myAccount, 2.5)
+addIncome(myAccount, 10)
+console.log(myAccount)
