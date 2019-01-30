205b14b62772c11fb59f0a2b451300f84eef8bf1
Pontus Svensson <Pontus.Svensson@hedinit.se>
2019-01-30 14:44:27 +0100

    object + challenge

---
+++ objects/objects-101.js
@@ -0,0 +1,31 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// Challenge area
+
+// Include name, age, location - object
+let person = {
+    name: 'Pontus',
+    age: 26,
+    location: 'Gothenburg'
+}
+
+// print it out
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+// increase age by 1 and print
+person.age += 1;
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+
+
+
