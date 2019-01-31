0db69e31bb1f7102a675ec2b4d18a8e8ae829bfd
Pontus Svensson <pontus.svensson@hedinit.se>
2019-01-31 08:27:53 +0100

    Expense Tracker

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,21 +1,44 @@
 let myAccount = {
     name: 'Pontus Svensson',
     expenses: 0,
-    income: 0
+    income: 0,
 }
 
 let addExpense = function (account, amount) {
     account.expenses = account.expenses + amount
-    console.log(account)
 }
 
 let addIncome = function (account, amount) {
     account.income = account.income + amount
-    console.log(account)
 }
 
-addExpense(myAccount, 2.5)
-addIncome(myAccount, 10)
-console.log(myAccount)
+// reset account function
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
 
-// test
+// get account summery (account name, balance, income, expenses)
+let accountSummary = function (account) {
+    let balance = account.income - account.expenses
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses`
+}
+
+// addIncome
+addIncome(myAccount, 10)
+
+// add Expense
+addExpense(myAccount, 5)
+
+// add Expense
+addExpense(myAccount, 2)
+
+// getAccountSummery
+let result = accountSummary(myAccount)
+console.log(result)
+// reset Account
+resetAccount(myAccount)
+
+// getAccountSummery
+let lastResult = accountSummary(myAccount)
+console.log(lastResult)
