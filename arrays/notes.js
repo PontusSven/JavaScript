2b4df633beee841df808539b3a57e4055eff297d
Pontus Svensson <pontus.svensson@hedinit.se>
2019-02-01 09:29:05 +0100

    array methods part 2

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,5 +1,28 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+/*
 console.log(notes.length)
 console.log(notes[0])
 console.log(notes[notes.length - 1])
+*/
+
+console.log(notes)
+
+// Array method
+/* notes.push('My new note')
+console.log(notes)
+
+notes.pop()
+console.log(notes)
+
+console.log(notes.shift())
+console.log(notes.unshift('new unshift'))
+console.log(notes) */
+
+// Splice
+notes.splice(1, 0, 'this is the new item')
+console.log(notes)
+
+// replace 
+notes[2] = 'this is now the new note 3'
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,10 +1,12 @@
-// Create array with 5 "todos"
-// you have x todos
-// print the first and the scond to last items --> "Todo: xxx"
-
 const todo = ['Clean', 'Train', 'Make dinner', 'Walk the dog', 'Buy groceriess']
 
-console.log(`You have ${todo.length} todos`)
+// delete the 3rd item
+todo.splice(2, 1)
+// add a new item onto the end
+todo.push('Wash dishes')
+// remove the first item from the list
+todo.shift()
 
-console.log(`First todo: ${todo[0]} `)
-console.log(`Second to last todo: ${todo[todo.length - 2]} `)
+
+console.log(`You have ${todo.length} todos`)
+console.log(todo)
