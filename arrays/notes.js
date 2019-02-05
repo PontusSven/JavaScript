39d51815b53d2f907833af89f60e85eb81ab6f09
Pontus Svensson <pontus.svensson@hedinit.se>
2019-02-05 09:42:17 +0100

    cleaned up notes

--- arrays/notes.js
+++ arrays/notes.js
@@ -23,53 +23,53 @@ const sortNotes = function () {
 }
 
 sortNotes(notes)
-console.log(notes)
+/* console.log(notes)
 
-/* console.log(notes.length)
+console.log(notes.length)
 console.log(notes[0])
 console.log(notes[notes.length - 1])
 
-console.log(notes)
+// console.log(notes)
 
 // Array method
 notes.push('My new note')
-console.log(notes)
+// console.log(notes)
 
 notes.pop()
-console.log(notes)
+/* console.log(notes)
 
 console.log(notes.shift())
 console.log(notes.unshift('new unshift'))
 console.log(notes)
-
+ */
 // Splice
 notes.splice(1, 0, 'this is the new item')
-console.log(notes)
+// console.log(notes)
 
 // replace 
 notes[2] = 'this is now the new note 3'
-console.log(notes)
+// console.log(notes)
 
 notes.forEach(function(item, index) {
-    console.log(index)
-    console.log(item)
+    // console.log(index)
+    // console.log(item)
 })
 
-*/
 
-/* for (let count = 0; count <= 2; count++) {
-    console.log(`Hello, ${count}`)
+
+for (let count = 0; count <= 2; count++) {
+    // console.log(`Hello, ${count}`)
 } 
 
 for (let count = notes.length - 1; count >= 0; count--) {
-    console.log(notes[count])
-} */
+    // console.log(notes[count])
+} 
 
-/* const index = notes.findIndex(function (note, index) {
-    console.log(note)
+const index = notes.findIndex(function (note, index) {
+    // console.log(note)
     return note.title == 'Habbits to work on'
 })
-console.log(index) */
+// console.log(index) 
 
 const findNote = function (notes, noteTitle) {
      return notes.find(function (note, index) {
@@ -86,15 +86,15 @@ const findNotes = function (notes, query) {
 }
 
 
-/* const findNote = function (notes, noteTitle) {
+const findNote = function (notes, noteTitle) {
     const index = notes.findIndex(function (note, index) {
        return note.title.toLowerCase() === noteTitle.toLowerCase()
     })
     return notes[index]
-} */
+} 
 
-/* const note = findNote(notes, 'Office improvements')
-console.log(note) */
+const note = findNote(notes, 'Office improvements')
+// console.log(note) 
 
 
 
