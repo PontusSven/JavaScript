81bf858b6061741afa47c3754d611a8b6442b617
Pontus Svensson <pontus.svensson@hedinit.se>
2019-02-05 13:17:48 +0100

    expense tracker part 2

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,6 +1,14 @@
 const account = {
     name: 'Pontus Svensson',
     expenses: [],
+    income: [],
+    // addIncome --> description, amount
+    AddIncome: function (description, amount) {
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
     // addExpense --> description, amount
     addExpense: function (description, amount) {
         this.expenses.push({
@@ -11,22 +19,24 @@ const account = {
     // getAccountSummary - total up all expenses --> Pontus Svensson has $1250 is expenses
     getAccountSummary: function () {
         let totalExpenses = 0
+        let totalIncome = 0
 
         this.expenses.forEach(function (expense) {
             totalExpenses += expense.amount
         })
-            return `${account.name} has $${totalExpenses} in expenses`
+        this.income.forEach(function (income) {
+            totalIncome += income.amount
+        })
+
+            return `${account.name} has a balance of $${totalIncome - totalExpenses}. $${totalIncome} in income. $${totalExpenses} in expenses`
         
     }
 }
 
-// Expense --> description, amount
-// getAccountSummary - total up all expenses --> Pontus Svensson has $1250 is expenses
-
-
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.AddIncome('Job', 1000)
 console.log(account.getAccountSummary())
 
 console.log(account)
