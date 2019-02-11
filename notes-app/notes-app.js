89fc6a21059ed2a90679305722d59cbdd2396986
Pontus Svensson <pontus.svensson@hedinit.se>
2019-02-11 15:15:42 +0100

    dropdown

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -11,7 +11,7 @@ const notes = [{
 
 
 const filters = {
-    searchText: ''
+    searchText: '' 
 }
 
 const renderNotes = function (notes, filters) {
@@ -40,7 +40,6 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
     renderNotes(notes, filters)
 })
 
-document.querySelector('#for-fun').addEventListener('change', function (e) {
-    console.log(e.target.checked)
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
 })
-
