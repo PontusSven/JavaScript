a3a34b9833ec3c7c5862288403fa3e8cf9aa4edb
Pontus Svensson <pontus.svensson@hedinit.se>
2019-02-13 10:55:42 +0100

    code override

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,70 +1,60 @@
-const todo = [{
-    text: 'Clean',
-    completed: true
-},{
-    text: 'Train',
-    completed: false
-},{
-    text: 'Make dinner',
-    completed: false
-},{
-    text: 'Walk the dog',
-    completed: true
-},{ 
-    text: 'Buy groceries',
-    completed: true
-}]
+let todos = []
 
 const filters = {
     searchText: '',
     hideCompleted: false
 }
 
-const renderTodos = function (todo, filters) {
-    const filteredTodos = todo.filter(function (todo) {
+const todosJSON = localStorage.getItem('todos')
+
+if (todosJSON !== null) {
+    todos = JSON.parse(todosJSON)
+}
+
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-        
+
         return searchTextMatch && hideCompletedMatch
     })
 
     const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !filteredTodos.completed
+        return !todo.completed
     })
 
-    document.querySelector('#todos-filter').innerHTML = ''
+    document.querySelector('#todos').innerHTML = ''
 
     const summary = document.createElement('h2')
     summary.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todos-filter').appendChild(summary)
-    
+    document.querySelector('#todos').appendChild(summary)
+
     filteredTodos.forEach(function (todo) {
-        const newTodo = document.createElement('p')
-        newTodo.textContent = todo.text
-        document.querySelector('#todos-filter').appendChild(newTodo)
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
     })
 }
 
-renderTodos(todo, filters)
-
+renderTodos(todos, filters)
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
-    renderTodos(todo, filters)
+    renderTodos(todos, filters)
 })
 
-document.querySelector('#todo-form').addEventListener('submit', function (e) {
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
     e.preventDefault()
-    todo.push({
-        text: e.target.elements.newTodo.value,
+    todos.push({
+        text: e.target.elements.text.value,
         completed: false
     })
-    renderTodos(todo, filters)
-    e.target.elements.newTodo.value = ''
-}) 
+    localStorage.setItem('todos', JSON.stringify(todos))
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
 
 document.querySelector('#hide-completed').addEventListener('change', function (e) {
-    filters.hideCompleted = e.target.value
-    renderTodos(todo, filters)
-
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
 })
